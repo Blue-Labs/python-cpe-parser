59648c0522de5c3115d3e37446fe157ce8d6fe24
David Ford <david@blue-labs.org>
2014-06-21 04:58:07 +0000

    markdown changes

--- cpe_parser.py
+++ cpe_parser.py
@@ -6,10 +6,10 @@ CPE tree parser
 given a block of HTML fetched from a CVE vulnerability page, parse the CPE vulnerability tree into a VTunit object.
 
 todo:
-  - sort CPE entries
-  - remove redundant entries
-  - build __lt__, __le__, __gt__, __ge__ methods for comparisons
-  - build a method for testing a <= b <= c
+* sort CPE entries
+* remove redundant entries
+* build __lt__, __le__, __gt__, __ge__ methods for comparisons
+* build a method for testing a <= b <= c
 
 
 i realize most people don't pay attention to licenses and requirements. i've no desire to harsh on people that
@@ -18,10 +18,10 @@ my copyright and disclaimer and please make attribution if you use my code. plea
 http://en.wikipedia.org/wiki/Apache_License
 
 requirements:
-  python3+         (there's really not much in here that is python3 dependent, probably easy to mod for py2)
-    beautifulsoup4
-      lxml or similar html parser that BS4 approves of
-    httplib2 - if you run this standalone. httplib2 is far more advanced than the builtin urllib/httplib combo
+*  python3+         (there's really not much in here that is python3 dependent, probably easy to mod for py2)
+  *  beautifulsoup4
+    *  lxml or similar html parser that BS4 approves of
+  *  httplib2 - if you run this standalone. httplib2 is far more advanced than the builtin urllib/httplib combo
 
 '''
 
